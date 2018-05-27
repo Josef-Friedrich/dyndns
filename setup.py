6fd79668872ec4ab4fad33abe14030ed379af085
Josef Friedrich <josef@friedrich.rocks>
2018-05-27 15:42:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="example_pkg",
+setup(
+    name="jfddns,
     version="0.0.1",
-    author="Example Author",
+    author="Josef Friedrich",
     author_email="author@example.com",
     description="A small example package",
     long_description=long_description,
