e91069fcf01141df988c6e980630feb8a369e0fd
Josef Friedrich <josef@friedrich.rocks>
2018-06-09 19:35:15 +0200

    Add more exceptions

--- jfddns/exceptions.py
+++ jfddns/exceptions.py
@@ -1,2 +1,14 @@
 class JfErr(Exception):
     pass
+
+
+class JfDDnsError(Exception):
+    """Base exception of the package ``jfddns``."""
+
+
+class ConfigurationError(JfDDnsError):
+    """Server side configuration error."""
+
+
+class ParameterError(JfDDnsError):
+    """Client side parameter error."""
