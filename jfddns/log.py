900791f02ad852138101da4dc516bfb7e4069229
Josef Friedrich <josef@friedrich.rocks>
2018-06-14 21:55:55 +0200

    Handler

--- jfddns/log.py
+++ jfddns/log.py
@@ -1,6 +1,9 @@
 """Bundles the logging functionality."""
 
 import logging
+import os
+
+log_file = os.path.join(os.getcwd(), 'jfddns.log')
 
 
 class Message(object):
@@ -13,29 +16,29 @@ class Message(object):
     # NOTSET 	0
 
     log_levels = {
-        'CONFIGURATION_ERROR': 49,
-        'DNS_SERVER_ERROR': 48,
-        'PARAMETER_ERROR': 39,
-        'UPDATED': 19,
-        'UNCHANGED': 9,
+        'CONFIGURATION_ERROR': 51,
+        'DNS_SERVER_ERROR': 51,
+        'PARAMETER_ERROR': 41,
+        'UPDATED': 21,
+        'UNCHANGED': 11,
     }
-
-    def _log_level_num(self, log_level):
-        return self.log_levels[log_level]
 
     def __init__(self):
         self._setup_logging()
+
+    def _log_level_num(self, log_level):
+        return self.log_levels[log_level]
 
     def _setup_logging(self):
         for log_level, log_level_num in self.log_levels.items():
             logging.addLevelName(log_level_num, log_level)
 
         self.logger = logging.getLogger('jfddns')
-        handler = logging.FileHandler('jfddns.log')
-        formatter = logging.Formatter('%(asctime)s %(message)s')
+        handler = logging.FileHandler(log_file)
+        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
         handler.setFormatter(formatter)
         self.logger.addHandler(handler)
-        self.logger.setLevel(logging.INFO)
+        self.logger.setLevel(logging.DEBUG)
 
     def message(self, msg, log_level):
         self.logger.log(self._log_level_num(log_level), msg)
--- test/test_log.py
+++ test/test_log.py
@@ -2,10 +2,27 @@ import unittest
 from jfddns import log
 
 
+def clean_log_file(log_file_path):
+    log_file = open(log_file_path, 'w')
+    log_file.write('')
+    log_file.close()
+
+
 class TestMethodMsg(unittest.TestCase):
+
+    def setUp(self):
+        self.log_file = log.log_file
+        clean_log_file(self.log_file)
 
     def test_msg(self):
         self.assertEqual(log.msg('lol', 'UNCHANGED'), 'UNCHANGED: lol')
+
+    def test_log_file(self):
+        log.msg('lol', 'UNCHANGED')
+        log_file = open(log.log_file, 'r')
+        result = log_file.read()
+        self.assertIn('UNCHANGED', result)
+        self.assertIn('lol', result)
 
 
 if __name__ == '__main__':
