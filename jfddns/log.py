2d41f52fab706039baef2fd86729245405c35afc
Josef Friedrich <josef@friedrich.rocks>
2018-06-14 17:31:40 +0200

    Extend logging

--- jfddns/log.py
+++ jfddns/log.py
@@ -14,3 +14,25 @@ def msg(text, level='info'):
     log_message = getattr(logger, level)
     log_message(text)
     return text
+
+
+def message(text, level='info'):
+    log_message = getattr(logger, level)
+    log_message(text)
+    return text
+
+
+CONFIGURATION_ERROR = 51
+logging.addLevelName(CONFIGURATION_ERROR, 'CONFIGURATION_ERROR')
+
+PARAMETER_ERROR = 41
+logging.addLevelName(PARAMETER_ERROR, 'PARAMETER_ERROR')
+
+DNS_SERVER_ERROR = 31
+logging.addLevelName(DNS_SERVER_ERROR, 'DNS_SERVER_ERROR')
+
+UPDATED = 21
+logging.addLevelName(UPDATED, 'UPDATED')
+
+UNCHANGED = 11
+logging.addLevelName(UNCHANGED, 'UNCHANGED')
