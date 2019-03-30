f490763266d6b6bdab3acfc4010674bf32224f30
Josef Friedrich <josef@friedrich.rocks>
2019-03-30 18:37:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
-from setuptools import setup, find_packages
 import os
+
+from setuptools import setup, find_packages
 import versioneer
 
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+def read(file_name):
+    """
+    Read the contents of a text file and return its content.
+
+    :param str file_name: The name of the file to read.
+
+    :return: The content of the text file.
+    :rtype: str
+    """
+    return open(
+        os.path.join(os.path.dirname(__file__), file_name),
+        encoding='utf-8'
+    ).read()
 
 
 setup(
@@ -33,12 +45,12 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    entry_points = {
+    entry_points={
         'console_scripts': [
             'dyndns-debug = dyndns.cli:main',
         ],
     },
-    package_data = {
+    package_data={
         '': ['rst/*.rst', 'templates/*.html', 'static/*'],
     },
 )
