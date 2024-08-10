0da9c3974923a471506b0e3a2b0b8c866ab8f3b6
Josef Friedrich <josef@friedrich.rocks>
2024-08-10 10:13:37 +0200

    Fix some tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,14 +4,14 @@ import subprocess
 class TestCli:
     def test_help(self) -> None:
         out = subprocess.run(
-            ["dyndns-debug", "--help"], encoding="utf-8", stdout=subprocess.PIPE
+            ["dyndns", "--help"], encoding="utf-8", stdout=subprocess.PIPE
         )
         assert out.returncode == 0
         assert out.stdout
 
     def test_version(self) -> None:
         out = subprocess.run(
-            ["dyndns-debug", "--version"], encoding="utf-8", stdout=subprocess.PIPE
+            ["dyndns", "--version"], encoding="utf-8", stdout=subprocess.PIPE
         )
         assert out.returncode == 0
         assert out.stdout
